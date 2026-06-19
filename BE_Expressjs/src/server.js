ee9047add9aadb096285d403367b3a35e012dbea
NMC2007 <nmc281207@gmail.com>
2026-06-20 00:03:35 +0700

    Establish a FE-BE connection

--- BE_Expressjs/src/server.js
+++ BE_Expressjs/src/server.js
@@ -3,6 +3,7 @@ import tasksRouter from './routes/tasksRouters.js'
 import authRouter from './routes/authRouters.js'
 import AppDataSource from './config/db.js'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ const app = express();
 
 // Middleware
 app.use(express.json());  // ← Thêm để parse JSON
+
+app.use(cors({ origin: ["http://localhost:5173"] }))
 
 // puplic routers
 app.use("/api/auth", authRouter)
